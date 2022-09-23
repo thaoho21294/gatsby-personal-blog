d8d553c3d93000ca239fd28a0e94d0571aeaad2a
Thao <thaoho21294@gmail.com>
2022-09-23 13:04:56 +0700

    Finished Gatsby Tutorial Part 7

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
       {children}
     </Layout>
   );
@@ -18,6 +22,14 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
